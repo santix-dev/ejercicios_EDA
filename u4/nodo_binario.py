ec8dcf752f615fc47e07630cb86ca5a20ef23337
santix-dev <santigimenez.20020817@gmail.com>
2023-09-15 17:31:34 -0300

    nodo para arbol binario listo

--- u4/nodo_binario.py
+++ u4/nodo_binario.py
@@ -1,3 +1,18 @@
-
+class Nodo_bin():
+    __dato=None
+    __ant=None
+    __sig=None
+    def __init__(self,dato):
+        self.__dato=dato
+    def set_siguiente(self,sig):
+        self.__sig=sig
+    def set_anterior(self,ant):
+        self.__ant=ant
+    def get_anterior(self):
+        return self.__ant
+    def get_siguiente(self):
+        return self.__sig
+    def dato(self):
+        return self.__dato
     
     
